b8484d22df333a8023df9593077c61da28b99a51
r0p0s3c <rudolph.pereira+github@occamsec.com>
2017-01-25 16:00:35 -0500

    fix typos for non-ansifilter cases

--- scripts/start_logging.sh
+++ scripts/start_logging.sh
@@ -12,16 +12,16 @@ system_osx() {
 }
 
 pipe_pane() {
-	tmux pipe-pane "exec cat - | $filter | ~/.tmux/plugins/tmux-logging/scripts/prepend-datetime.sh >> $FILE"
+	tmux pipe-pane "exec cat - | stdbuf -o0 -e0 $filter | ~/.tmux/plugins/tmux-logging/scripts/prepend-datetime.sh >> $FILE"
 }
 
 start_pipe_pane() {
 	if ansifilter_installed; then
 		local filter="ansifilter";
 	elif system_osx; then
-		local filter=sed -E 's/(\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]||]0;[^]+|[[:space:]]+$)//g'
+		local filter="sed -E 's/(\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]||]0;[^]+|[[:space:]]+$)//g'"
 	else
-		local filter=sed -e 's/(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]|)//g'
+		local filter="sed -r -e 's/(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]|)//g'"
 	fi
 	pipe_pane
 }
